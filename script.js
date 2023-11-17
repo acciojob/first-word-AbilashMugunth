25e36650c2a1a99c1e39d92d6dfe4c1e84ae65ee
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-17 12:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+	  if (str == "") {
+		 return str;
+	  }
+
+	  let indexOfSpace = str.indexOf(" ");
+	  if (indexOfSpace == -1) return str;
+		return str.slice(0, indexOfSpace); 
 }
+
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
